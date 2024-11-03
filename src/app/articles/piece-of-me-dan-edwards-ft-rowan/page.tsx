9fbb5b60a10bbc0948c9c93c18b4ef7e6b35dbc7
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-03 10:39:25 +0000

    Create temporary articles

---
+++ src/app/articles/piece-of-me-dan-edwards-ft-rowan/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from 'next'
+
+import { generateArticleMetadata } from '@/library/metadata'
+import { ArticleLayout } from '@/components/ArticleLayout'
+import { article } from './data'
+import { Paragraph } from '@/components/Text'
+import PreSaveCard from '../../../components/PreSaveCard'
+
+export const generateMetadata = (): Metadata => {
+  return generateArticleMetadata(article)
+}
+
+export default function Page() {
+  return (
+    <ArticleLayout article={article}>
+      <Paragraph>This is a paragraph</Paragraph>
+      <PreSaveCard />
+    </ArticleLayout>
+  )
+}
