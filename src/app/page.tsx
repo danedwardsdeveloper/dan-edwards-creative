fb792dc6332c12450c63816872bce94ac5786348
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-03 10:40:44 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,36 @@
-import { type Metadata } from 'next';
+import { type Metadata } from 'next'
 
-import { Container } from '@/components/Container';
-import { getAllArticles } from '@/library/articles';
-import ArticlesList from '@/components/ArticlesList';
+// import ArticlesList from '@/components/ArticlesList'
+import { Container } from '@/components/Container'
+// import { getAllArticles } from '@/library/articles'
+import { productionBaseURL } from '@/library/environment'
+import PreSaveCard from '@/components/PreSaveCard'
 
 export const metadata: Metadata = {
-	title: `Dan Digresses`,
-	// ToDo
-	description: ``,
-};
+  title: `Dan Edwards creative | Pop music producer and songwriter in Salisbury, UK.`,
+  description: `Dan Edwards is a classically trained professional pop music producer and song writer.`,
+  alternates: {
+    canonical: productionBaseURL,
+  },
+}
 
-export default async function ProjectsPage() {
-	const articles = await getAllArticles();
+export default async function Home() {
+  // const articles = await getAllArticles()
 
-	return (
-		<Container className="mt-16 sm:mt-32">
-			<header className="max-w-2xl">
-				<h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100 text-balance">
-					{`Dan Digresses`}
-				</h1>
-				<p className="mt-6 text-base text-zinc-600 dark:text-zinc-400 text-balance">
-					{`I'm `}
-					<em>{`always`}</em>
-					{` busy with a project. I've been programming for nearly a year now, and I've made lots of different sites, apps and scripts of varying scales and complexities. Here are my favorite ones, plus my work in progress and ambitious plans for the future.`}
-				</p>
-			</header>
-			<div className="mt-16 sm:mt-20">
-				<ArticlesList articles={articles} />
-			</div>
-		</Container>
-	);
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <header className="max-w-2xl">
+        <h1 className="text-balance text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+          {`Dan Edwards`}
+        </h1>
+        <p className="mt-6 text-balance text-base text-zinc-600 dark:text-zinc-400">
+          {`Pop music producer & songwriter in Salisbury, UK.`}
+        </p>
+      </header>
+      <div className="mt-16 sm:mt-20">
+        <PreSaveCard />
+        {/* <ArticlesList articles={articles} /> */}
+      </div>
+    </Container>
+  )
 }
