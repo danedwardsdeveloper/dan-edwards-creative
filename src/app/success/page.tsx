be8b6bb38f6286aed64a94574749990d540713ad
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-03 10:44:30 +0000

    Create page.tsx

---
+++ src/app/success/page.tsx
@@ -0,0 +1,10 @@
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+export default function SuccessPage() {
+  return (
+    <SimpleLayout
+      title="Success!"
+      intro="Thank you for pre-saving my new song. Have a lovely day!"
+    ></SimpleLayout>
+  )
+}
