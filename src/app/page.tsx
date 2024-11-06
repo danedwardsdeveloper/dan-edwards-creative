3580398adc1df54534517d1799755f5079b90afb
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-06 22:39:34 +0000

    Use TwoColumnLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import { type Metadata } from 'next'
 
-// import ArticlesList from '@/components/ArticlesList'
-import { Container } from '@/components/Container'
-// import { getAllArticles } from '@/library/articles'
+import ArticlesList from '@/components/ArticlesList'
+import { getAllArticles } from '@/library/articles'
 import { productionBaseURL } from '@/library/environment'
 import PreSaveCard from '@/components/PreSaveCard'
+import TwoColumnLayout from '@/components/TwoColumnLayout'
 
 export const metadata: Metadata = {
   title: `Dan Edwards creative | Pop music producer and songwriter in Salisbury, UK.`,
@@ -15,22 +15,14 @@ export const metadata: Metadata = {
 }
 
 export default async function Home() {
-  // const articles = await getAllArticles()
+  const articles = await getAllArticles()
 
   return (
-    <Container className="mt-16 sm:mt-32">
-      <header className="max-w-2xl">
-        <h1 className="text-balance text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          {`Dan Edwards`}
-        </h1>
-        <p className="mt-6 text-balance text-base text-zinc-600 dark:text-zinc-400">
-          {`Pop music producer & songwriter in Salisbury, UK.`}
-        </p>
-      </header>
-      <div className="mt-16 sm:mt-20">
-        <PreSaveCard />
-        {/* <ArticlesList articles={articles} /> */}
-      </div>
-    </Container>
+    <TwoColumnLayout
+      title={`Dan Edwards`}
+      intro={`Pop music producer & songwriter in Salisbury, UK.`}
+      columnOne={<ArticlesList articles={articles} />}
+      columnTwo={<PreSaveCard />}
+    />
   )
 }
