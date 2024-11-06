dea440f7768d333f66489e0484816b64abe35478
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-06 22:29:27 +0000

    Add audio preview, style fixes

--- src/components/PreSaveCard.tsx
+++ src/components/PreSaveCard.tsx
@@ -10,8 +10,8 @@ import { EpisodePlayButton } from './EpisodePlayButton'
 import { type Episode } from './AudioProvider'
 
 const trackPreview: Episode = {
-  title: `Piece of Me - Dan Edwards (ft. rowan)`,
-  slug: 'piece-of-me',
+  title: `Piece of Me - Dan Edwards (ft. rowan) [clip]`,
+  slug: 'piece-of-me-clip-mq',
 }
 
 export default function PreSaveCard({ classes }: { classes?: string }) {
@@ -37,20 +37,21 @@ export default function PreSaveCard({ classes }: { classes?: string }) {
         alt={`Piece of Me by Dan Edwards (ft. rowan) cover art`}
         className="rounded-md"
         priority
+        placeholder="blur"
       />
       <EpisodePlayButton
         episode={trackPreview}
-        className="flex items-center gap-x-3 text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900"
+        className="flex items-center gap-x-3 pt-4 text-sm font-bold leading-6 text-emerald-500 hover:text-emerald-700 active:text-emerald-900 dark:text-emerald-300 dark:hover:text-emerald-500 dark:active:text-emerald-500"
         playing={
           <>
             <PauseIcon className="h-2.5 w-2.5 fill-current" />
-            <span aria-hidden="true">Listen</span>
+            <span aria-hidden="true">Listen to preview</span>
           </>
         }
         paused={
           <>
             <PlayIcon className="h-2.5 w-2.5 fill-current" />
-            <span aria-hidden="true">Listen</span>
+            <span aria-hidden="true">Listen to preview</span>
           </>
         }
       />
@@ -58,14 +59,14 @@ export default function PreSaveCard({ classes }: { classes?: string }) {
         <p className="text-sm text-slate-600 dark:text-slate-300">{`'Pre-saving' a song on Spotify means the track will appear in your  'Liked songs' playlist as soon as it's released. You'll need a Spotify account - free or premium - and you'll need to sign in to Spotify to authorise the pre-save on the next page.`}</p>
         <Button
           onClick={handlePreSave}
-          className="flex justify-between"
+          className="flex justify-start text-sm sm:justify-between sm:text-base"
           variant="green"
         >
           <SpotifyIcon className="h-10 w-10" colour="black" />
           <span className="ml-2 text-base font-semibold">
             Pre-Save on Spotify
           </span>
-          <div className="h-10 w-10" />
+          <div className="hidden h-10 w-10 sm:block" />
         </Button>
         <p className="text-slate-900 dark:text-slate-100">{`Available everywhere Friday 8 November`}</p>
       </div>
