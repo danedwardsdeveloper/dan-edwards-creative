b51fb172836e3d4910ed1ceb5b118710a9300829
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-18 19:42:34 +0000

    Create manifest.ts

---
+++ src/app/manifest.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Dan Edwards creative | Pop music producer and songwriter in Salisbury, UK.',
+    short_name: 'Dan Edwards creative',
+    description:
+      'Dan Edwards creative | Pop music producer and songwriter in Salisbury, UK.',
+    start_url: '/',
+    display: 'standalone',
+
+    // Light gray for the splash screen
+    // background_color: '#F8F8F8',
+
+    // Black text
+    // theme_color: '#FFFFFF',
+    icons: [
+      // {
+      // 	src: '/favicon/icon-192.png',
+      // 	sizes: '192x192',
+      // 	type: 'image/png',
+      // },
+      {
+        src: '/favicon/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
