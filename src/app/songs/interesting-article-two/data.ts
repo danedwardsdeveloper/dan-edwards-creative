036d65d4c88d51761e76ac536535fb20336108f5
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-18 19:42:54 +0000

    Create /songs directory

--- src/app/songs/interesting-article-two/data.ts
+++ src/app/songs/interesting-article-two/data.ts
@@ -9,12 +9,12 @@ export const generateMetadata = (): Metadata => {
 }
 
 export const article: Article = {
-  title: 'This is in the articles directory!',
+  title: 'This is in the songs directory!',
   display: true,
   displayDescription:
-    'An iOS app to help people keep track of the number of plant foods they consume.',
+    'An iOS app to help people keep track of the number of plant foods they consume. An iOS app to help people keep track of the number of plant foods they consume.',
   metaDescription:
     'An iPhone app that helps people to keep track of the number of plant foods they consume, built with React Native and Expo.',
-  date: '2024-09-30',
+  date: '2020-01-01',
   socialImage: plantCounterSocialImage,
 }
