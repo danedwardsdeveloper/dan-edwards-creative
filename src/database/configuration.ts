6ef43b2d4d1d21b28016394941ce40217aa14e9a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-19 21:55:26 +0000

    Create configuration.ts

---
+++ src/database/configuration.ts
@@ -0,0 +1,8 @@
+export const databaseNames = 'dan-edwards-creative'
+
+export const tableNames = {
+  pageViews: 'page-views',
+  preSaves: 'pre-saves',
+  linkClicks: 'link-clicks',
+  subscriptions: 'subscriptions',
+}
