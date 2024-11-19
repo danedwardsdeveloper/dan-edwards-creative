21037e0adf3b487853d05a0aacae1d3c6a11c75f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-19 20:21:46 +0000

    Refactor styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,15 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 
+const footerLinkStyles = clsx(
+  'text-sm',
+  'text-zinc-500 dark:text-zinc-400',
+  'decoration-transparent dark:decoration-zinc-400',
+  'underline-offset-2',
+  'hover:underline hover:decoration-zinc-500',
+  'transition-all duration-300',
+)
+
 export function Footer() {
   return (
     <footer
@@ -15,15 +24,12 @@ export function Footer() {
     >
       <Link
         href="/privacy-policy-and-terms-of-service"
-        className={clsx(
-          'text-sm',
-          'text-zinc-500 dark:text-zinc-400',
-          'decoration-transparent dark:decoration-zinc-400',
-          'underline-offset-2',
-          'hover:underline hover:decoration-zinc-500',
-          'transition-all duration-300',
-        )}
+        className={footerLinkStyles}
       >{`Privacy policy & terms of service`}</Link>
+      <p className="text-sm text-zinc-500 dark:text-zinc-400">
+        {`Site by `}
+        <Link href="https://danedwardsdeveloper.com/" className={footerLinkStyles}>{`Dan Edwards`}</Link>
+      </p>
       <p className="text-sm text-zinc-500 dark:text-zinc-400">© {new Date().getFullYear()}, Dan Edwards</p>
     </footer>
   )
