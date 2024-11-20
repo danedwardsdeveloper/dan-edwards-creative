e7f24a8ba41bb582c96c77b16af3b31381db082e
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-20 16:58:30 +0000

    Add ToDo

--- src/library/environment.ts
+++ src/library/environment.ts
@@ -4,4 +4,5 @@ export const isDevelopment = process.env.NODE_ENV === 'development'
 export const productionBaseURL = 'https://danedwardscreative.com'
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
+// ToDo
 export const spotifyRedirectUri = `${dynamicBaseURL}/api/spotify/callback`
