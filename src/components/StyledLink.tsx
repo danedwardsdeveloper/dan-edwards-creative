712eb810f784dbf5e12948b931bb870f902e395d
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-21 09:29:12 +0000

    Change colour

--- src/components/StyledLink.tsx
+++ src/components/StyledLink.tsx
@@ -20,8 +20,8 @@ export default function StyledLink({ href, ariaLabel, children, classes }: Style
       aria-label={ariaLabel}
       title={ariaLabel}
       className={clsx(
-        'text-blue-600 hover:text-blue-400',
-        'dark:text-blue-500 dark:hover:text-blue-700',
+        'text-slate-600 hover:text-slate-400',
+        'dark:text-slate-500 dark:hover:text-slate-700',
         'transition-all duration-200',
         'underline underline-offset-2 decoration-slate-300 dark:decoration-slate-600',
         classes,
