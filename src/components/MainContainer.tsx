8afe97d311479a372738a4717c154722d03920bf
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-21 09:30:40 +0000

    Update MainContainer.tsx

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -21,6 +21,7 @@ export default function MainContainer({ children }: Props) {
       <div
         className={clsx(
           'pt-20 md:pt-0 overflow-y-auto transition-all duration-300',
+          'bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20',
           showAudioPlayer ? 'mb-96' : 'mb-32',
         )}
         style={{
@@ -35,4 +36,18 @@ export default function MainContainer({ children }: Props) {
       <AudioPlayerContainer />
     </>
   )
+}
+
+{
+  /* <div className="fixed inset-0 flex justify-center sm:px-8">
+        <div className="flex w-full max-w-7xl lg:px-8">
+          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+        </div>
+      </div>
+      <div className="relative flex w-full flex-col">
+        <Header />
+        <main className="flex-auto">{children}</main>
+        <Footer />
+      </div>
+    </> */
 }
