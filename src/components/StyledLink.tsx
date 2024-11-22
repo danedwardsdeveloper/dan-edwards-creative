23490756a15b46934aa0635e5fc4b3280b459b7b
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-22 10:46:23 +0000

    Add colour options

--- src/components/StyledLink.tsx
+++ src/components/StyledLink.tsx
@@ -4,15 +4,47 @@ import { ReactNode } from 'react'
 
 interface StyledLinkProps {
   href: string
+  text?: string
+  colour?: 'default' | 'red' | 'green' | 'blue'
   ariaLabel?: string
-  children: ReactNode
+  children?: ReactNode
   classes?: string
 }
 
-export default function StyledLink({ href, ariaLabel, children, classes }: StyledLinkProps) {
+export default function StyledLink({
+  href,
+  text,
+  colour = 'default',
+  ariaLabel,
+  children,
+  classes,
+}: StyledLinkProps) {
   const isExternal = href.startsWith('http') || href.startsWith('//')
 
   const targetRelationProps = isExternal ? { target: '_blank', rel: 'noopener noreferrer' } : {}
+
+  const colourStyles = {
+    default: {
+      text: 'text-slate-600 hover:text-slate-500 dark:text-slate-400 dark:hover:text-slate-300',
+      decoration:
+        'decoration-slate-400 hover:decoration-slate-300 dark:decoration-slate-600 dark:hover:decoration-slate-500',
+    },
+    red: {
+      text: 'text-red-600 hover:text-red-500 dark:text-red-400 dark:hover:text-red-300',
+      decoration:
+        'decoration-red-400 hover:decoration-red-300 dark:decoration-red-600 dark:hover:decoration-red-500',
+    },
+    green: {
+      text: 'text-green-600 hover:text-green-500 dark:text-green-400 dark:hover:text-green-300',
+      decoration:
+        'decoration-green-400 hover:decoration-green-300 dark:decoration-green-600 dark:hover:decoration-green-500',
+    },
+    blue: {
+      text: 'text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300',
+      decoration:
+        'decoration-blue-400 hover:decoration-blue-300 dark:decoration-blue-600 dark:hover:decoration-blue-500',
+    },
+  }
 
   return (
     <Link
@@ -20,15 +52,15 @@ export default function StyledLink({ href, ariaLabel, children, classes }: Style
       aria-label={ariaLabel}
       title={ariaLabel}
       className={clsx(
-        'text-slate-600 hover:text-slate-400',
-        'dark:text-slate-500 dark:hover:text-slate-700',
+        colourStyles[colour].text,
+        colourStyles[colour].decoration,
         'transition-all duration-200',
-        'underline underline-offset-2 decoration-slate-300 dark:decoration-slate-600',
+        'underline underline-offset-2',
         classes,
       )}
       {...targetRelationProps}
     >
-      {children}
+      {text || children}
     </Link>
   )
 }
