37d539dceaad620628d5aca49696848c6c2ecbb6
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-22 16:00:28 +0000

    Move sliding container, update margins

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -16,11 +16,12 @@ export default function MainContainer({ children }: Props) {
   const { showAudioPlayer } = useLayout()
 
   return (
-    <>
+    <div id="sliding-container" className="w-full transition-transform duration-500 ">
       <MenuBars />
       <div
         className={clsx(
-          'pt-20 md:pt-0 overflow-y-auto transition-all duration-300',
+          'flex flex-col pt-20 md:pt-0 overflow-y-auto transition-all duration-300',
+          'md:mx-8 lg:mx-16',
           'bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20',
           showAudioPlayer ? 'mb-96' : 'mb-32',
         )}
@@ -28,26 +29,12 @@ export default function MainContainer({ children }: Props) {
           height: showAudioPlayer ? 'calc(100vh - 116px)' : '100vh',
         }}
       >
-        <main role="main" className="flex flex-1 flex-col min-h-full mx-auto max-w-2xl mt-6 md:mt-12">
+        <main role="main" className="flex flex-1 flex-col mx-auto max-w-2xl md:mt-20">
           {children}
         </main>
         <Footer />
       </div>
       <AudioPlayerContainer />
-    </>
+    </div>
   )
-}
-
-{
-  /* <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-        </div>
-      </div>
-      <div className="relative flex w-full flex-col">
-        <Header />
-        <main className="flex-auto">{children}</main>
-        <Footer />
-      </div>
-    </> */
 }
