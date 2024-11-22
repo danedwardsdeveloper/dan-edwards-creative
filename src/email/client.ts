15d877e6361acd6fd2ff15efd20189614d5b2b8b
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-22 14:50:04 +0000

    Rename file

--- src/email/client.ts
+++ src/email/client.ts
@@ -2,12 +2,16 @@ import { SESClient } from '@aws-sdk/client-ses'
 
 const keyId = process.env.AWS_ACCESS_KEY_ID
 const secretKey = process.env.AWS_SECRET_ACCESS_KEY
+export const fromEmail = process.env.SES_FROM_EMAIL
 
 if (!keyId) {
   throw new Error('AWS_ACCESS_KEY_ID missing')
 }
 if (!secretKey) {
   throw new Error('AWS_SECRET_ACCESS_KEY missing')
+}
+if (!process.env.SES_FROM_EMAIL) {
+  throw new Error('SES_FROM_EMAIL missing')
 }
 
 export const sesClient = new SESClient({
