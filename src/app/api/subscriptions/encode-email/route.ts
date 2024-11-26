6f204e1afc2744e4569acc6afd15a59239c10409
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 21:47:34 +0000

    Change plainEmail to email

--- src/app/api/subscriptions/encode-email/route.ts
+++ src/app/api/subscriptions/encode-email/route.ts
@@ -6,14 +6,14 @@ import { encodeEmail } from '../utilities'
 
 export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.url)
-  const plainEmail = searchParams.get('e')
+  const email = searchParams.get('e')
 
-  if (!plainEmail) {
+  if (!email) {
     logger.error(`Missing param: 'e'`)
     return NextResponse.json({ error: `Missing Param: 'e'` }, { status: 400 })
   }
 
-  const encodedEmail = encodeEmail(plainEmail)
+  const encodedEmail = encodeEmail(email)
 
   return NextResponse.json({ message: `Encoded email: ${encodedEmail}` }, { status: 200 })
 }
