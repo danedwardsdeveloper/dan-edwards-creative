066a6cb1feb52f34d637c9d2902ede81719485bf
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 10:21:45 +0000

    Update api responses

--- src/app/api/subscriptions/unsubscribe/route.ts
+++ src/app/api/subscriptions/unsubscribe/route.ts
@@ -1,47 +1,64 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+import { Subscriber } from '@/library/database/models/subscriber'
+import connectDB from '@/library/database/mongoose'
 import { logger } from '@/library/logger'
 
-import { type SubscriptionStatus } from '../types'
-import { tableNames } from '@/database/configuration'
-import mongoClient from '@/database/mongodb'
+import { decodeEmail } from '../utilities'
+import { ApiEndpoints } from '@/types/apiEndpoints'
 
-export async function PATCH(request: NextRequest) {
+export async function DELETE(
+  request: NextRequest,
+): Promise<NextResponse<ApiEndpoints['/api/subscriptions/unsubscribe']['DELETE']['response']>> {
   try {
-    const { searchParams } = new URL(request.url)
-    const token = searchParams.get('token')
+    await connectDB()
 
-    if (!token) {
-      return NextResponse.json({ error: 'Unsubscribe token is required' }, { status: 400 })
+    const { searchParams } = new URL(request.url)
+    const typesafeParams = Object.fromEntries(
+      searchParams.entries(),
+    ) as ApiEndpoints['/api/subscriptions/unsubscribe']['DELETE']['params']
+    const unsubscribeToken = typesafeParams.x
+    const encodedEmail = typesafeParams.e
+
+    if (!unsubscribeToken) {
+      logger.error(`Missing param: 'x'`)
+      return NextResponse.json({ message: 'Invalid link' }, { status: 400 })
     }
 
-    const collection = (await mongoClient).db().collection(tableNames.subscriptions)
-    const subscriber = await collection.findOne({ unsubscribeToken: token })
+    if (!encodedEmail) {
+      logger.error(`Missing param: 'e'`)
+      return NextResponse.json({ message: 'Invalid link' }, { status: 400 })
+    }
+
+    const email = decodeEmail(encodedEmail)
+
+    const subscriber = await Subscriber.findOne({
+      $or: [{ email }, { unsubscribeToken }],
+    })
 
     if (!subscriber) {
-      return NextResponse.json({ error: 'Invalid unsubscribe token' }, { status: 404 })
+      return NextResponse.json(
+        {
+          message: 'User already unsubscribed',
+        },
+        { status: 404 },
+      )
     }
 
-    if (subscriber.status === 'unsubscribed') {
-      return NextResponse.json({
-        message: `${subscriber.email} already unsubscribed`,
-        subscriber,
-      })
+    if (subscriber.unsubscribeToken !== unsubscribeToken || subscriber.email !== email) {
+      return NextResponse.json({ message: 'Invalid link' }, { status: 400 })
     }
 
-    const result = await collection.findOneAndUpdate(
-      { unsubscribeToken: token },
-      { $set: { status: 'unsubscribed' as SubscriptionStatus } },
-      { returnDocument: 'after' },
-    )
+    await Subscriber.deleteOne({
+      email,
+      unsubscribeToken,
+    })
 
     return NextResponse.json({
-      message: `${subscriber.email} unsubscribed successfully`,
-      subscriber: result,
+      message: `${email} unsubscribed successfully`,
     })
   } catch (error) {
-    logger.error('Failed to unsubscribe:')
-    logger.error(error as string)
-    return NextResponse.json({ error: 'Failed to unsubscribe' }, { status: 500 })
+    logger.error('Failed to unsubscribe:', error instanceof Error ? error.message : String(error))
+    return NextResponse.json({ message: 'Failed to unsubscribe' }, { status: 500 })
   }
 }
