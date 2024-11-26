0176e1bc37c5057809129b5c547ab4360c5e45ed
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 10:20:25 +0000

    Move files

--- src/database/configuration.ts
+++
@@ -1,8 +0,0 @@
-export const databaseNames = 'dan-edwards-creative'
-
-export const tableNames = {
-  pageViews: 'page-views',
-  preSaves: 'pre-saves',
-  linkClicks: 'link-clicks',
-  subscriptions: 'subscriptions',
-}
