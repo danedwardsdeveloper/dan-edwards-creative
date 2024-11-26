77e1b990dd3790f41bde6ae8a55409993734fcaf
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 10:14:47 +0000

    Create styles.ts

---
+++ src/library/email/styles.ts
@@ -0,0 +1,9 @@
+export const zinc500 = '#71717a'
+export const zinc700 = '#3f3f46'
+export const zinc900 = '#18181b'
+
+export const slate200 = '#e2e8f0'
+
+export const blue500 = '#3B82F6'
+
+export const fontBase = '18px'
