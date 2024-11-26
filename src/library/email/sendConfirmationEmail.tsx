1a23e131d0e8b613f1160443a6e8392eebbfe193
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 21:43:34 +0000

    Add confirmation link

--- src/library/email/sendConfirmationEmail.tsx
+++ src/library/email/sendConfirmationEmail.tsx
@@ -1,12 +1,21 @@
 import { type SubscriberInterface } from '../database/models/subscriber'
 import { sendEmail } from './sendEmail'
+import { generateConfirmationLink } from '@/app/api/subscriptions/utilities'
 
-export async function sendConfirmationEmail(subscriber: SubscriberInterface) {
+export function generateConfirmationEmailContent(subscriber: SubscriberInterface) {
+  const confirmationLink = generateConfirmationLink(subscriber.confirmationToken, subscriber.email)
+
   const content = [
     `<p>Hey there${subscriber.firstName && `, ${subscriber.firstName}`}</p>`,
     `<p>Please confirm your subscription:</p>`,
+    confirmationLink,
     `<p>If you didn't request this subscription, you can safely ignore this email.</p>`,
   ]
+  return content
+}
+
+export async function sendConfirmationEmail(subscriber: SubscriberInterface) {
+  const content = generateConfirmationEmailContent(subscriber)
 
   return sendEmail({
     subscriber,
