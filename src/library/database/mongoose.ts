f3a3bb1bdea91694f418bab0e3da1735f02355e8
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 10:19:21 +0000

    Create mongoose.ts

---
+++ src/library/database/mongoose.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+import { logger } from '../logger'
+
+mongoose.set('strictQuery', true)
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI missing')
+    }
+
+    await mongoose.connect(process.env.MONGODB_URI)
+    logger.info('MongoDB connected via Mongoose')
+  } catch (error) {
+    logger.info('MongoDB connection error:', error)
+    process.exit(1)
+  }
+}
+
+export default connectDB
