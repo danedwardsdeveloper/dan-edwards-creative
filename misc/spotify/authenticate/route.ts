f873f31fd67a11469872b0267a80ad845699be54
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 10:22:37 +0000

    Move files

---
+++ misc/spotify/authenticate/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+
+import { spotifyRedirectUri } from '@/library/environment'
+
+export async function GET() {
+  const spotifyClientId = process.env.SPOTIFY_CLIENT_ID
+  const scope = 'user-library-modify user-library-read'
+  const state = Math.random().toString(36).substring(7)
+
+  const authUrl = new URL('https://accounts.spotify.com/authorize')
+  authUrl.searchParams.append('response_type', 'code')
+  authUrl.searchParams.append('client_id', spotifyClientId!)
+  authUrl.searchParams.append('scope', scope)
+  authUrl.searchParams.append('redirect_uri', spotifyRedirectUri)
+  authUrl.searchParams.append('state', state)
+
+  return NextResponse.json({ url: authUrl.toString() })
+}
