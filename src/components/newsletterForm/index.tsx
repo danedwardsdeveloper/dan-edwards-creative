d6fb8b848effa031e94514ffb8d45ed3c28a955f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 23:09:39 +0000

    Use typesafeFetch

--- src/components/newsletterForm/index.tsx
+++ src/components/newsletterForm/index.tsx
@@ -1,20 +1,18 @@
 import clsx from 'clsx'
 import { ReactNode, useState } from 'react'
 
-import { logger } from '@/library/logger'
+import { typesafeFetch } from '@/library/typesafeFetch'
 
 import { Button } from '../Button'
 import Spinner from '../Spinner'
 import StyledLink from '../StyledLink'
 import Input from './Input'
-import { ApiEndpoints, ApiPath } from '@/types/apiEndpoints'
+import { ApiEndpoints } from '@/types/apiEndpoints'
 
-interface MessageConfig {
-  heading: string
-  text: string | ReactNode
-  colour: string
-}
-type MessageStatus = 'default' | 'success' | 'subscribed' | 'pending' | 'error'
+type MessageStatus = Exclude<
+  ApiEndpoints['/api/subscriptions/add']['POST']['data']['message'],
+  'email required' | 'first name required'
+>
 
 export function NewsletterForm() {
   const [isSubscribing, setIsSubscribing] = useState(false)
@@ -22,28 +20,74 @@ export function NewsletterForm() {
   const [email, setEmail] = useState('')
   const [status, setStatus] = useState<MessageStatus>('default')
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setIsSubscribing(true)
+
+    try {
+      const response = await typesafeFetch({
+        path: '/api/subscriptions/add',
+        method: 'POST',
+        body: { email: email.trim(), firstName: firstName.trim() },
+      })
+
+      switch (response.message) {
+        case 'success please confirm':
+          setStatus('success please confirm')
+          break
+        case 'already pending':
+          setStatus('already pending')
+          break
+        case 'already confirmed':
+          setStatus('already confirmed')
+          break
+        default:
+          throw new Error('Subscription failed')
+      }
+    } catch {
+      setStatus('fail')
+    } finally {
+      setIsSubscribing(false)
+    }
+  }
+
+  const formIsVisible = status === 'default' || status === 'fail'
+
+  const greenText = 'text-green-600 dark:text-green-400'
+
+  interface MessageConfig {
+    heading: string
+    text: string | ReactNode
+    colour: string
+  }
+
   const messageConfigs: Record<MessageStatus, MessageConfig> = {
     default: {
       heading: 'Join my mailing list',
       text: 'I agree to receive emails. Unsubscribe with one click anytime.',
       colour: 'text-slate-600 dark:text-slate-400',
     },
-    success: {
+    'success please confirm': {
       heading: `Thank you${firstName ? `, ${firstName}` : ''}`,
       text: 'Please click the link in your email to confirm your subscription. Remember to check your junk folder.',
-      colour: 'text-green-600 dark:text-green-400',
+      colour: greenText,
     },
-    subscribed: {
+    'success confirmation bypassed': {
+      heading: `Thank you${firstName ? `, ${firstName}` : ''}`,
+      text: `${email ? email : `You're `} successfully subscribed to my newsletter.`,
+      colour: greenText,
+    },
+    'already confirmed': {
       heading: `Thank you${firstName ? `, ${firstName}` : ''}`,
       text: `${email ? email : `You're `} already subscribed successfully.`,
-      colour: 'text-green-600 dark:text-green-400',
+      colour: greenText,
     },
-    pending: {
+    'already pending': {
       heading: 'Awaiting confirmation',
       text: `Please click the link in the confirmation email${email ? ` sent to ${email}` : ''}, which may be in your junk folder.`,
       colour: 'text-orange-600 dark:text-orange-400',
     },
-    error: {
+    fail: {
       heading: 'Sorry, something went wrong',
       text: (
         <>
@@ -53,49 +97,6 @@ export function NewsletterForm() {
       colour: 'text-red-600 dark:text-red-400',
     },
   }
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    setIsSubscribing(true)
-
-    const path = '/api/subscriptions/add' as ApiPath
-
-    try {
-      const response = await fetch(path, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: email.trim(), firstName: firstName.trim() }),
-      })
-
-      logger.info('Received subscription response', {
-        status: response.status,
-      })
-
-      const data: ApiEndpoints['/api/subscriptions/add']['POST']['response'] = await response.json()
-      switch (response.status) {
-        case 201:
-        case 200:
-          setStatus('success')
-          break
-        case 202:
-          setStatus('pending')
-          break
-        case 409:
-          setStatus('subscribed')
-          break
-        default:
-          throw new Error('Subscription failed')
-      }
-    } catch {
-      setStatus('error')
-    } finally {
-      setIsSubscribing(false)
-    }
-  }
-
-  const formIsVisible = ['default', 'error'].includes(status)
 
   return (
     <form onSubmit={handleSubmit} className="max-w-md space-y-4 border border-gray-200 rounded-xl p-4">
