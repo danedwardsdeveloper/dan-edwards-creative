4bfd3cef06e09bd9bf7d0bf2c6ce95f600f2278a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-26 23:09:59 +0000

    Temporarily disable

--- src/hooks/usePageView.ts
+++ src/hooks/usePageView.ts
@@ -1,30 +1,28 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { useEffect } from 'react'
+// import { usePathname } from 'next/navigation'
+// import { useEffect } from 'react'
 
-import { typesafeFetch } from '@/types/apiEndpoints'
+// import { typesafeFetch } from '@/types/typesafeFetch'
 
 export function usePageView() {
-  const pathname = usePathname()
-
-  useEffect(() => {
-    const countView = async () => {
-      try {
-        await typesafeFetch({
-          path: '/api/analytics/page-views',
-          method: 'POST',
-          body: {
-            page: pathname,
-          },
-        })
-      } catch (error) {
-        console.error('Error logging page view:', error)
-      }
-    }
-
-    if (pathname) {
-      countView()
-    }
-  }, [pathname])
+  // const pathname = usePathname()
+  //   useEffect(() => {
+  //     const countView = async () => {
+  //       try {
+  //         await typesafeFetch({
+  //           path: '/api/analytics/page-views',
+  //           method: 'POST',
+  //           body: {
+  //             page: pathname,
+  //           },
+  //         })
+  //       } catch (error) {
+  //         console.error('Error logging page view:', error)
+  //       }
+  //     }
+  //     if (pathname) {
+  //       countView()
+  //     }
+  //   }, [pathname])
 }
