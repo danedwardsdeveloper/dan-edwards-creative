95d12adb68bc3b9556f264d70b0412d2f94a9846
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 10:11:46 +0000

    Restore file

--- src/hooks/useRecordLinkClick.ts
+++ src/hooks/useRecordLinkClick.ts
@@ -1,37 +1,59 @@
-// 'use client'
+'use client'
 
-// import { usePathname } from 'next/navigation'
-// import { useCallback } from 'react'
+import { usePathname } from 'next/navigation'
+import { useCallback } from 'react'
 
-// // import { Destination } from '@/app/api/analytics/link-clicks/route'
-// import { ApiEndpoints, ApiMethod, ApiPath } from '@/types/apiEndpoints'
+import { Destination } from '@/library/database/models/linkClick'
+import logger from '@/library/logger'
+import { typesafeFetch } from '@/library/typesafeFetch'
 
-// export function useRecordLinkClick() {
-//   const currentPath = usePathname()
+export function useRecordLinkClick() {
+  const currentPath = usePathname()
 
-//   const recordClick = useCallback(
-//     async (destination: Destination) => {
-//       try {
-//         const typesafePath: ApiPath<'/api/analytics/link-clicks'> = '/api/analytics/link-clicks'
-//         const typesafeMethod: ApiMethod<'/api/analytics/link-clicks'> = 'POST'
-//         const typesafeBody: ApiEndpoints['/api/analytics/link-clicks']['POST']['body'] = {
-//           destination,
-//           source: currentPath,
-//         }
+  const recordClick = useCallback(
+    async (destination: Destination) => {
+      logger.debug('Current path: ', currentPath)
+      logger.debug('Destination: ', destination)
 
-//         await fetch(typesafePath, {
-//           method: typesafeMethod,
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify(typesafeBody),
-//         })
-//       } catch (error) {
-//         console.error('Error logging link click:', error)
-//       }
-//     },
-//     [currentPath],
-//   )
+      try {
+        const data = await typesafeFetch({
+          path: '/api/analytics/link-clicks',
+          method: 'POST',
+          body: {
+            destination,
+            source: currentPath,
+          },
+        })
 
-//   return recordClick
-// }
+        switch (data.message) {
+          case 'destination required':
+            logger.info('Destination required')
+            break
+          case 'source required':
+            logger.info('Source required')
+            break
+          case 'admin click failed':
+            logger.error('Admin click failed to record')
+            break
+          case 'admin click recorded':
+            logger.info('Admin click successfully recorded')
+            break
+          case 'click failed':
+            logger.error('Click failed to record')
+            break
+          case 'click recorded':
+            logger.info('Click recorded successfully')
+            break
+          default:
+            logger.error('Unknown response from analytics endpoint')
+            break
+        }
+      } catch (error) {
+        logger.error('Error logging link click:', error)
+      }
+    },
+    [currentPath],
+  )
+
+  return recordClick
+}
