68daaf35c06a7897874f8eb1867cf36fa57f6560
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 16:43:19 +0000

    decodeEmail returns empty string on error

--- src/app/api/subscriptions/utilities.ts
+++ src/app/api/subscriptions/utilities.ts
@@ -1,6 +1,7 @@
 import crypto from 'crypto'
 
 import { dynamicBaseURL } from '@/library/environment'
+import logger from '@/library/logger'
 
 export function generateToken(): string {
   return crypto.randomBytes(10).toString('hex')
@@ -11,12 +12,25 @@ export function encodeEmail(email: string): string {
 }
 
 export function decodeEmail(encodedEmail: string): string {
-  const base64 = encodedEmail
-    .replace(/-/g, '+')
-    .replace(/_/g, '/')
-    .padEnd(encodedEmail.length + ((4 - (encodedEmail.length % 4)) % 4), '=')
+  try {
+    const base64 = encodedEmail
+      .replace(/-/g, '+')
+      .replace(/_/g, '/')
+      .padEnd(encodedEmail.length + ((4 - (encodedEmail.length % 4)) % 4), '=')
 
-  return Buffer.from(base64, 'base64').toString()
+    const decoded = Buffer.from(base64, 'base64').toString()
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(decoded)) {
+      logger.info('Invalid email format')
+      return ''
+    }
+
+    return decoded
+  } catch (error) {
+    logger.info('Failed to decode email: ', { error })
+    return ''
+  }
 }
 
 export function generateConfirmationURL(token: string, email: string): string {
