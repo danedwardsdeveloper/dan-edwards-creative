0ba46af80d6e31ad6f114275d74a65551b8ffb0c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 10:16:37 +0000

    Update index.tsx

--- src/components/menuBars/desktopMenu/index.tsx
+++ src/components/menuBars/desktopMenu/index.tsx
@@ -1,22 +1,33 @@
 'use client'
 
+import clsx from 'clsx'
+
 import { menuItems } from '../data'
 import { ThemeToggleIcons } from '../ThemeToggle'
 import NavItem from './desktopMenuItems'
 
 export default function DesktopMenu() {
   return (
-    <div className="flex justify-center">
-      <nav className="flex items-center rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 pointer-events-auto">
-        <ul className="flex">
-          {menuItems.map(menuItem => (
-            <NavItem href={menuItem.href} key={menuItem.href}>
-              {menuItem.displayName}
-            </NavItem>
-          ))}
-        </ul>
-        <ThemeToggleIcons />
-      </nav>
+    <div
+      data-component="DesktopMenu"
+      className={clsx(
+        'h-16 pt-6',
+        'hidden md:block fixed',
+        'pt-6 left-0 right-0 z-50 top-0 w-full transition-transform duration-500',
+      )}
+    >
+      <div className="flex justify-center">
+        <nav className="flex items-center rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 pointer-events-auto">
+          <ul className="flex">
+            {menuItems.map(menuItem => (
+              <NavItem href={menuItem.href} key={menuItem.href}>
+                {menuItem.displayName}
+              </NavItem>
+            ))}
+          </ul>
+          <ThemeToggleIcons />
+        </nav>
+      </div>
     </div>
   )
 }
