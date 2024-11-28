28ec17000e06a9dbb5e4fc886d3789e83eb6c700
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 16:40:29 +0000

    Add NODE_ENV check

--- src/library/environment.ts
+++ src/library/environment.ts
@@ -1,8 +1,15 @@
-export const isProduction = process.env.NODE_ENV === 'production'
-export const isDevelopment = process.env.NODE_ENV === 'development'
+const nodeEnv = process.env.NODE_ENV
 
-// ToDo: Remember to change this!
+if (!nodeEnv) {
+  throw new Error('NODE_ENV missing')
+}
+if (nodeEnv !== 'production' && nodeEnv !== 'development') {
+  throw new Error(`NODE_ENV set to ${nodeEnv}, not 'development' or 'production'`)
+}
+
+export const isProduction = nodeEnv === 'production'
+export const isDevelopment = nodeEnv === 'development'
+
 export const productionBaseURL = 'https://dec-test.fly.dev'
 export const developmentBaseURL = 'http://localhost:3000'
 export const dynamicBaseURL = isProduction ? productionBaseURL : developmentBaseURL
-// export const spotifyRedirectUri = `${dynamicBaseURL}/api/spotify/callback`
