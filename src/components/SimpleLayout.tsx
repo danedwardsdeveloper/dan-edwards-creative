132960e5725b33be4b16a31bd6254c20f4858007
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 10:12:29 +0000

    Remove x margins

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -16,16 +16,14 @@ export function SimpleLayout({
 }) {
   return (
     <Container>
-      <header className="mx-auto max-w-2xl mt-12 px-4 md:px-0">
-        <h1 className="text-balance text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+      <header className="max-w-2xl mt-12">
+        <h1 className="text-balance text-4xl font-bold tracking-tight text-zinc-800  dark:text-zinc-100">
           {title}
         </h1>
         <p className="mt-6 text-balance text-base text-zinc-600 dark:text-zinc-400">{intro}</p>
       </header>
       {children && (
-        <div className={clsx('my-12 sm:my-20 px-4 md:px-0', articleContent && 'article-content')}>
-          {children}
-        </div>
+        <div className={clsx('my-12 sm:my-20', articleContent && 'article-content')}>{children}</div>
       )}
     </Container>
   )
