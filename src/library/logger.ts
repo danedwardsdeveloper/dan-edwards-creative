6659976db23159f9c74937d48bfc7d8aff2db062
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 10:45:16 +0000

    Format production output

--- src/library/logger.ts
+++ src/library/logger.ts
@@ -1,11 +1,25 @@
 import pino from 'pino'
 import { Logger } from 'pino'
 
+import { isProduction, logLevel } from './environment'
+
 const logger: Logger = pino({
-  transport: {
-    target: 'pino-pretty',
-  },
-  level: process.env.PINO_LOG_LEVEL || 'debug',
+  transport: isProduction
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: false,
+          messageFormat: '{level}: {msg}',
+          ignore: 'pid,hostname,time',
+        },
+      }
+    : {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      },
+  level: logLevel || 'debug',
   formatters: {
     level: label => {
       return { level: label.toUpperCase() }
@@ -14,5 +28,7 @@ const logger: Logger = pino({
   timestamp: false,
   base: null,
 })
+
+logger.info(`Pino logger set to ${logLevel}`)
 
 export default logger
