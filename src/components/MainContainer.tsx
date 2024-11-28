d5b004b279d7668f6519fe31255bc73e641f5c1e
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-28 10:17:29 +0000

    Update MainContainer.tsx

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,40 +1,26 @@
 'use client'
 
 import clsx from 'clsx'
-
-import AudioPlayerContainer from '@/components/audioPlayerContainer/index'
+import { ReactNode } from 'react'
 
 import { Footer } from './Footer'
-import MenuBars from './menuBars'
-import { useLayout } from '@/providers/layout'
 
-interface Props {
-  children: React.ReactNode
-}
-
-export default function MainContainer({ children }: Props) {
-  const { showAudioPlayer } = useLayout()
-
+export default function MainContainer({ children }: { children: ReactNode }) {
   return (
-    <div id="sliding-container" className="w-full transition-transform duration-500 ">
-      <MenuBars />
-      <div
-        className={clsx(
-          'flex flex-col pt-20 md:pt-0 overflow-y-auto transition-all duration-300',
-          'md:mx-8 lg:mx-16',
-          'bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20',
-          showAudioPlayer ? 'mb-96' : 'mb-32',
-        )}
-        style={{
-          height: showAudioPlayer ? 'calc(100vh - 116px)' : '100vh',
-        }}
-      >
-        <main role="main" className="flex flex-1 flex-col mx-auto max-w-2xl md:mt-20">
+    <main data-component="MainContainer" className="flex-1 overflow-y-auto w-full">
+      <div className="min-h-full flex flex-col lg:mx-16 bg-white">
+        <div
+          className={clsx(
+            'md:mt-16', // Offset DesktopMenu. MobileMenu is part of the flex container while DesktopMenu is position:absolute
+            'flex-1 max-w-4xl mx-auto px-6',
+          )}
+        >
           {children}
-        </main>
-        <Footer />
+        </div>
+        <div data-component="FooterContainer">
+          <Footer />
+        </div>
       </div>
-      <AudioPlayerContainer />
-    </div>
+    </main>
   )
 }
