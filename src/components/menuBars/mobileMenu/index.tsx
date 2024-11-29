f306778f3d0930b24458a18dedb99544cf1e0df4
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-29 14:32:44 +0000

    Remove hide on scroll

--- src/components/menuBars/mobileMenu/index.tsx
+++ src/components/menuBars/mobileMenu/index.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import clsx from 'clsx'
-import { ElementRef, useEffect, useRef } from 'react'
+import { after } from 'node:test'
 
+// import { ElementRef, useEffect, useRef } from 'react'
 import Divider from '@/components/Divider'
 
 import { ThemeToggleButtons } from '../ThemeToggle'
@@ -10,47 +11,49 @@ import SiteLinks from './Links'
 import { HomeButton, MenuButton } from './MenuButtons'
 import { useLayout } from '@/providers/layout'
 
+// import { useLayout } from '@/providers/layout'
+
 export default function MobileMenu() {
-  const ref = useRef<ElementRef<'div'>>(null)
-  const lastScrollTop = useRef(0)
+  // const ref = useRef<ElementRef<'div'>>(null)
+  // const lastScrollTop = useRef(0)
   const { setMenusVisible, mobilePanelVisible, menusVisible } = useLayout()
 
-  useEffect(() => {
-    const scrollContainer = document.querySelector('[data-component="MainContainer"]') as HTMLElement
+  // useEffect(() => {
+  //   const scrollContainer = document.querySelector('[data-component="MainContainer"]') as HTMLElement
 
-    if (!scrollContainer) return
+  //   if (!scrollContainer) return
 
-    function updateHeaderStyles() {
-      if (mobilePanelVisible) {
-        setMenusVisible(true)
-        return
-      }
+  //   function updateHeaderStyles() {
+  //     if (mobilePanelVisible) {
+  //       setMenusVisible(true)
+  //       return
+  //     }
 
-      const scrollTop = scrollContainer.scrollTop
-      const isAtBottom = scrollContainer.scrollHeight - scrollContainer.clientHeight <= scrollTop + 10
+  //     const scrollTop = scrollContainer.scrollTop
+  //     const isAtBottom = scrollContainer.scrollHeight - scrollContainer.clientHeight <= scrollTop + 10
 
-      if (!isAtBottom) {
-        if (scrollTop > lastScrollTop.current) {
-          setMenusVisible(false)
-        } else {
-          setMenusVisible(true)
-        }
-      }
+  //     if (!isAtBottom) {
+  //       if (scrollTop > lastScrollTop.current) {
+  //         setMenusVisible(false)
+  //       } else {
+  //         setMenusVisible(true)
+  //       }
+  //     }
 
-      lastScrollTop.current = scrollTop
-    }
+  //     lastScrollTop.current = scrollTop
+  //   }
 
-    const handleScroll = () => requestAnimationFrame(updateHeaderStyles)
+  //   const handleScroll = () => requestAnimationFrame(updateHeaderStyles)
 
-    scrollContainer.addEventListener('scroll', handleScroll, {
-      passive: true,
-    })
-    return () => scrollContainer.removeEventListener('scroll', handleScroll)
-  }, [mobilePanelVisible, setMenusVisible])
+  //   scrollContainer.addEventListener('scroll', handleScroll, {
+  //     passive: true,
+  //   })
+  //   return () => scrollContainer.removeEventListener('scroll', handleScroll)
+  // }, [mobilePanelVisible, setMenusVisible])
 
   return (
     <header
-      ref={ref}
+      // ref={ref}
       data-component="MobileMenu"
       className={clsx(
         'md:hidden',
@@ -58,11 +61,14 @@ export default function MobileMenu() {
         'transition-all duration-500 transform-gpu',
         'after:absolute after:bottom-0 after:left-0 after:right-0',
         'after:bg-slate-300 after:transition-all after:duration-500',
-        menusVisible
-          ? mobilePanelVisible
-            ? 'h-[405px] after:bottom-0 after:h-[2px]'
-            : 'h-[87px] after:bottom-0 after:h-[2px]'
-          : 'h-0 after:h-0',
+        // menusVisible
+        // ? mobilePanelVisible
+        // ? 'h-[405px] after:bottom-0 after:h-[2px]'
+        // : 'h-[87px] after:bottom-0 after:h-[2px]'
+        // : 'h-0 after:h-0',
+        mobilePanelVisible
+          ? 'h-[405px] after:bottom-0 after:h-[2px]'
+          : 'h-[87px] after:bottom-0 after:h-[2px]',
       )}
       role="banner"
       aria-label="Mobile navigation menu"
