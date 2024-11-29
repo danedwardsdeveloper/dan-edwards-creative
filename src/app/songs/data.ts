6c10118f08c869a3101eb67d1ef966a800a6ab80
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-29 15:31:15 +0000

    Add release date & isReleased function

--- src/app/songs/data.ts
+++ src/app/songs/data.ts
@@ -1,6 +1,16 @@
-import { type Song } from '@/providers/audio'
+export interface Song {
+  title: string
+  slug: string
+  releaseDate: Date
+}
 
 export const chewingGum: Song = {
   title: 'Chewing Gum (ft. rowan) - Dan Edwards [PREVIEW]',
   slug: 'chewing-gum',
+  releaseDate: new Date('2025-01-10T00:00:00Z'),
+}
+
+export function isReleased({ song }: { song: Song }) {
+  const now = new Date()
+  return now.getTime() > song.releaseDate.getTime()
 }
