d06e4028351becd9895cb356d8216884b57547e5
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-29 15:30:23 +0000

    Show at all breakpoints

--- src/components/menuBars/desktopMenu/index.tsx
+++ src/components/menuBars/desktopMenu/index.tsx
@@ -12,12 +12,12 @@ export default function DesktopMenu() {
       data-component="DesktopMenu"
       className={clsx(
         'h-16 pt-6',
-        'hidden md:block fixed',
+        'block fixed',
         'pt-6 left-0 right-0 z-40 top-0 w-full transition-transform duration-500',
       )}
     >
       <div className="flex justify-center">
-        <nav className="flex items-center rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 pointer-events-auto">
+        <nav className="flex  items-center rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 pointer-events-auto">
           <ul className="flex">
             {menuItems.map(menuItem => (
               <NavItem href={menuItem.href} key={menuItem.href}>
