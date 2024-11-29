4f0622ccbb63b14b56eed0cab9103513d55c1b71
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-29 15:29:10 +0000

    Update font size

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -17,7 +17,7 @@ export function SimpleLayout({
   return (
     <Container>
       <header className="max-w-2xl mt-12">
-        <h1 className="text-balance text-4xl font-bold tracking-tight text-zinc-800  dark:text-zinc-100">
+        <h1 className="text-balance text-5xl font-bold tracking-tight text-zinc-800  dark:text-zinc-100">
           {title}
         </h1>
         <p className="mt-6 text-balance text-base text-zinc-600 dark:text-zinc-400">{intro}</p>
