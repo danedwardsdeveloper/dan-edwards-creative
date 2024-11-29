7db0b50df1bf581d3c04d937684df4d1c62c9fb7
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-29 15:28:47 +0000

    Add Header

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,16 +1,18 @@
 'use client'
 
 import ObfuscatedEmail from '../privacy-policy-and-terms-of-service/components/ObfuscatedEmail'
+import { Header } from '@/components/Header'
 import { NewsletterForm } from '@/components/newsletterForm'
 import TwoColumnLayout from '@/components/TwoColumnLayout'
 
 export default function ContactPage() {
   return (
-    <TwoColumnLayout
-      title={'Contact'}
-      intro={`If you'd like to work with me or have a question, I'd love to hear from you.`}
-      columnOne={<ObfuscatedEmail />}
-      columnTwo={<NewsletterForm />}
-    />
+    <>
+      <Header
+        title={'Contact'}
+        intro={`If you'd like to work with me or have a question, I'd love to hear from you.`}
+      />
+      <TwoColumnLayout columnOne={<ObfuscatedEmail />} columnTwo={<NewsletterForm />} />
+    </>
   )
 }
