1bdb9e2681cf3b09528c166accc0856a6a813179
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-29 15:29:00 +0000

    Update focus styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,7 +19,8 @@ type ButtonProps = {
 
 export function Button({ variant = 'primary', className, ...props }: ButtonProps) {
   className = clsx(
-    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-4 transition active:transition-none w-full',
+    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm  transition active:transition-none w-full',
+    'outline-offset-4 focus:outline-blue-500',
     'w-full',
     variantStyles[variant],
     className,
