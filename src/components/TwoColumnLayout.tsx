c4fc061c59dcc3fed69ecb1d8e11a6e350d33b01
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-01 10:57:47 +0000

    Remove mx-auto

--- src/components/TwoColumnLayout.tsx
+++ src/components/TwoColumnLayout.tsx
@@ -9,7 +9,7 @@ export default function TwoColumnLayout({
     <div data-component="TwoColumnLayout">
       <div className="grid grid-cols-1 gap-x-20 lg:grid-cols-[1fr_400px] gap-y-28 lg:px-8">
         <div>{columnOne}</div>
-        <div className="mx-auto">{columnTwo}</div>
+        <div>{columnTwo}</div>
       </div>
     </div>
   )
