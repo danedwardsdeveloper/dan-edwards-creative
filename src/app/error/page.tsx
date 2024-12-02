9cb0b3c4af48557676a386c8b56c81a5650fe7d6
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-02 14:11:07 +0000

    Use ErrorButtons

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -3,19 +3,21 @@
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 
-import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
+import ErrorButtons from '@/components/ErrorButtons'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+import { usePreviousPathname } from '@/providers/previousPathname'
 
 function ErrorContent() {
   const searchParams = useSearchParams()
   const errorMessage = searchParams.get('message') || 'An error occurred'
+  const previousPathname = usePreviousPathname()
 
   return (
-    <div className="mx-auto mt-12 max-w-sm">
-      <h1 className="mb-4 text-2xl font-bold text-red-500">Error</h1>
-      <p className="mb-6 text-slate-700 dark:text-slate-300">{errorMessage}</p>
-      <Button onClick={() => window.history.back()}>Back</Button>
-    </div>
+    <SimpleLayout title="Error" intro={errorMessage}>
+      <ErrorButtons path={previousPathname} />
+    </SimpleLayout>
   )
 }
 
