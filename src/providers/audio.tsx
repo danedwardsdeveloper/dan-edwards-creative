7f0d80fe6f5bbbaf4cb8901c5f1e7fb04cff2ac1
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-02 14:08:16 +0000

    Move Song interface and details

--- src/providers/audio.tsx
+++ src/providers/audio.tsx
@@ -4,8 +4,24 @@ import { createContext, useContext, useMemo, useReducer, useRef, useState } from
 
 import logger from '@/library/logger'
 
-import { chewingGum, Song } from '@/app/songs/data'
 import { useRecordLinkClick } from '@/hooks/useRecordLinkClick'
+
+export interface Song {
+  title: string
+  slug: string
+  releaseDate: Date
+}
+
+export const chewingGum: Song = {
+  title: 'Chewing Gum (ft. rowan) - Dan Edwards [PREVIEW]',
+  slug: 'chewing-gum',
+  releaseDate: new Date('2025-01-10T00:00:00Z'),
+}
+
+export function isReleased({ song }: { song: Song }) {
+  const now = new Date()
+  return now.getTime() > song.releaseDate.getTime()
+}
 
 interface PlayerState {
   playing: boolean
