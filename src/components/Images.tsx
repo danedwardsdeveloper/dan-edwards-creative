6aa6ae20957535be9ee12dc148cf42b6c91143e8
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-04 16:08:51 +0000

    Configure for remote images

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,25 +1,28 @@
 import clsx from 'clsx'
-import Image, { StaticImageData } from 'next/image'
+import Image from 'next/image'
 import { ReactNode } from 'react'
 
 const baseContainerStyles = `w-full`
 const marginStyles = 'mt-10 mb-4'
 const baseImageStyles = `rounded-lg w-full h-auto`
 const borderStyles = 'border border-zinc-200 dark:border-zinc-700 rounded-lg'
-const sizes = '(max-width: 1023px) calc(100vw - 2rem), min(calc(100vw - 2rem), 576px)'
 const captionStyles = 'mt-2 mb-6 text-gray-600 italic text-sm'
 
 interface Props {
-  src: StaticImageData
+  src: string
   alt: string
+  height: number
+  width: number
+  sizes?: string
   caption?: string
   children?: ReactNode
   border?: boolean
   classes?: string
   margin?: boolean
+  priority?: boolean
 }
 
-export const FeaturedImage = ({ src, alt, border }: Props) => {
+export const FeaturedImage = ({ src, alt, sizes, border }: Props) => {
   return (
     <div className={clsx('w-full', marginStyles)}>
       <Image
@@ -36,15 +39,29 @@ export const FeaturedImage = ({ src, alt, border }: Props) => {
   )
 }
 
-export const AdditionalImage = ({ src, alt, caption, children, classes, border, margin = true }: Props) => {
+export const AdditionalImage = ({
+  src,
+  alt,
+  height,
+  width,
+  sizes,
+  caption,
+  children,
+  classes,
+  border,
+  margin = true,
+  priority,
+}: Props) => {
   return (
     <>
       <div className={clsx(baseContainerStyles, margin && marginStyles)}>
         <Image
           src={src}
           alt={alt}
+          height={height}
+          width={width}
           sizes={sizes}
-          placeholder="blur"
+          priority={priority}
           className={clsx(baseImageStyles, 'object-cover', classes, border && borderStyles)}
         />
       </div>
