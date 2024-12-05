ac475ea89ac2e416ff3c2319d9f586aac7ccadde
Dan Edwards <daniel.edwards96@yahoo.com>
2024-12-05 09:59:21 +0000

    Disable logs in production

--- src/library/logger.ts
+++ src/library/logger.ts
@@ -1,10 +1,16 @@
 /* eslint-disable no-console */
+import { isProduction } from './environment'
+
 type LogArgs = unknown[]
+
+const noop = (): void => {}
+
 const logger = {
-  debug: (...args: LogArgs): void => console.debug('[debug]', ...args),
-  info: (...args: LogArgs): void => console.info('[info]', ...args),
-  warn: (...args: LogArgs): void => console.warn('[warn]', ...args),
+  debug: isProduction ? noop : (...args: LogArgs): void => console.debug('[debug]', ...args),
+  info: isProduction ? noop : (...args: LogArgs): void => console.info('[info]', ...args),
+  warn: isProduction ? noop : (...args: LogArgs): void => console.warn('[warn]', ...args),
   error: (...args: LogArgs): void => console.error('[error]', ...args),
 } as const
+
 export type Logger = typeof logger
 export default logger
